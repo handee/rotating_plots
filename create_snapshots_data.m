a734d4152b7f1efe45532ad4884b13ddb2aeeb25
Hannah <hmd1@aber.ac.uk>
2018-11-09 09:08:50 +0000

    creating rotating plots from data

---
+++ create_snapshots_data.m
@@ -0,0 +1,40 @@
+clear all; 
+
+filename="rgb2.txt"
+data=dlmread(filename)
+
+
+
+redval=data(:,1)./255;
+blueval=data(:,2)./255;
+greenval=data(:,3)./255;
+colors=[redval';greenval';blueval'];
+disp(colors)
+hsvcolors=rgb2hsv(colors)
+disp(data(:,1))
+
+hsv255=hsvcolors./255
+
+
+# create 360 snapshots (1 per degree) 
+figure(1); 
+t = 100; 
+for degree=1:360
+  # clear graph 
+  clf; 
+# plot 3d snapshot with small dots "." 
+  scatter3(data(:,1)', data(:,2)', data(:,3)', [], colors', "filled"); 
+
+# configure label names and axis intervals [0, 1] 
+  xlabel("Red"); 
+  ylabel("Green"); 
+  zlabel("Blue"); 
+  axis([0 255 0 255 0 255]); 
+
+# rotate the graph according to current degree 
+  view(degree, 30 + 15 * sin(degree/60)); 
+# save the current snapshot file 
+  plotname = ['scatter_t', num2str(t), '.png']; 
+  print(plotname, '-dpng'); t = t + 1; 
+  disp(t);
+end;
